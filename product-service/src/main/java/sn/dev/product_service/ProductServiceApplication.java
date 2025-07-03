2009dec229f7fc2137fb9c4c9bd66da27ab76818
TanakAiko <cherifmbaye02@gmail.com>
2025-07-03 13:23:41 +0000

    Refactor security configuration and remove unused JWT filter

--- product-service/src/main/java/sn/dev/product_service/ProductServiceApplication.java
+++ product-service/src/main/java/sn/dev/product_service/ProductServiceApplication.java
@@ -2,12 +2,16 @@ package sn.dev.product_service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+
+import sn.dev.product_service.config.RSAKeysConfig;
 
 @SpringBootApplication
 @EnableDiscoveryClient
 @EnableFeignClients
+@EnableConfigurationProperties(RSAKeysConfig.class)
 public class ProductServiceApplication {
 
 	public static void main(String[] args) {
